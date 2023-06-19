61a5b8eaced5b7c1ba7756b9f7630f797b5c1259
Landy510 <randy.chen@tpisoftware.com>
2023-06-19 10:20:43 +0800

    add: header's styles

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,8 +1,25 @@
 import styles from './Header.module.scss';
 export default function Header() {
+  console.log('styles?', styles)
   return (
     <>
-      <h1>This is header</h1>
+      <header className={[styles['header'], 'bg-primary'].join(' ')}>
+        <h1>
+          <a 
+            title='Ramble CrossFit'
+          >
+            <img 
+              src="/src/assets/images/logo.png" 
+              alt="logo-image" 
+              height={60}
+            />
+            <span>Ramble CrossFit</span>
+          </a>
+        </h1>
+        <ul className={styles['options']}>
+          <li className={[styles['option'], 'fw-medium', 'text-h4'].join(' ')}>products</li>
+        </ul>
+      </header>
     </>
   )
 }
